3680a643e4f37ca8986be403fbc38b8f90af608a
Лаисат <lz_256@mail.ru>
2023-11-04 17:55:57 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+
     }
+    public static void task1() {
+        var age = 16;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен" + age
+                    + " , то он не достиг совершеннолетия, нужно немного подождать");
+
+        } else {
+            System.out.println("Если возраст человека равен"
+                    + age + " , то он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+
+    public static void task4() {
+
+    }
+
+    public static void task5() {
+
+    }
+
+    public static void task6() {
+
+    }
+
+    public static void task7() {
+
+    }
+
 }
