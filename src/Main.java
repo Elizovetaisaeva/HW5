32c812b55de6d1b20acbc335ca6dec63525f621a
Лаисат <lz_256@mail.ru>
2023-11-04 21:05:23 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,6 @@ public class Main {
                     + age + " , то он не достиг совершеннолетия, нужно немного подождать");
         }
     }
-
     public static void task2() {
         var temp = 3;
         if (temp < 5) {
@@ -30,7 +29,6 @@ public class Main {
         }
 
     }
-
     public static void task3() {
         var speed = 55;
         if (speed < 60) {
@@ -51,11 +49,9 @@ public class Main {
             } else if (age <= 24) {
                 System.out.println("Если возраст человека равен" + age + ", то ему нужно работать");
             }
-
     }
-
     public static void task5() {
-                age = 28;
+                var age = 28;
                 if ( age<= 5) {
                     System.out.println("Если возраст ребенка равен " +age+ ", то ему нельзя кататься на аттракционе");
                 } else if (age <= 14) {
@@ -64,34 +60,29 @@ public class Main {
                     System.out.println("Если возраст человека равен" +age+ ", то емубез сопровождения взрослого");
 
                 }
-
     }
-
     public static void task6() {
                 var people = 73;
-                var freeSpase = 60 - people;
-                var freeSpase = 102 - people;
-                if (freeSpase > 0) {
-                    System.out.println("Свободных сидячих мест" +freeSpase);
-                } else if (freeSpase > 0) {
-                    System.out.println("Есть сидячие места" +freeSpase);
+                var freeSeats = 60 - people;
+                var freeSpace = 102 - people;
+                if (freeSeats > 0) {
+                    System.out.println("Свободных сидячих мест" +freeSeats);
+                } else if (freeSpace > 0) {
+                    System.out.println("Есть сидячие места" +freeSpace);
                 } else  {
-                    System.out.println("Сидячих мест нет" +freeSpase);
+                    System.out.println("Сидячих мест нет" +freeSpace);
                 }
-
-
-
     }
-
-    public static void task7()
-            int one = 3;
-            int two = 7;
-            int three = -1;
-            if (one > two && one > three) {
-                System.out.println("One is the biggest. volue =" +one);
-            } else if (two > three) {
-                System.out.println("One is the biggest. volue =" + two);
-            } else {
-                System.out.println("One is the biggest. volue =" + three);
-            }
+    public static void task7() {
+        int one = 3;
+        int two = 7;
+        int three = -1;
+        if (one > two && one > three) {
+            System.out.println("One is the biggest. volue =" + one);
+        } else if (two > three) {
+            System.out.println("One is the biggest. volue =" + two);
+        } else {
+            System.out.println("One is the biggest. volue =" + three);
         }
+    }
+    }
